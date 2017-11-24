8d00e327fba5bf70a3e6eae3d4593fde931989a1
Ryan P <ryan.eliopoulos@gmail.com>
2017-11-23 19:40:54 -0800

    Extracts characters. Now need to begin storing them in a string

--- extractstring.c
+++ extractstring.c
@@ -11,15 +11,20 @@ void main(){
 	while(c != EOF){
 	
 		//preprocesses c: screens for ignore characters, all whitespace = 1 space, and all alpha characters are returned capitalized
-		c = preprocess(c);	
-				
-		if((isphrasebeginner) && (!phraseStarted)){
+	
+		c = preprocess(c);
+			
+		printf("%c   %d\n ", c, c);				
+
+		//Determines if c begins a new phrase
+		if((isphrasebeginner(c)) && (!phraseStarted)){
 			phraseStarted = 1;
 			printf("\\\\Beginning phrase \\\\\n");
 			printf("%c", c);
 			//string.append(c)
 			
 		}
+		//Determines if the current phrase is continuing
 		else if((phraseStarted) && (!isphraseender(c))){
 			//Accounts for the empty return preprocess gives when the character is to be ignored
 			if(c != 001){
@@ -29,15 +34,22 @@ void main(){
 			
 
 		}
+		//Determines if c ends the current phrase
 		else if(isphraseender(c)){
 			phraseStarted = 0;
+			//string.append(c)
 			printf("\\\\Phrase has endeded \\\\\n");
 		}
+		//If the last thing typed before pressing enter is a phrase ending character, there must be a carriage return or something read afterward that gets read after the phrase ends. Here lies that whitespace. RIP
+		else if(c == ' '){
+			printf("c is a rebel without  cause\n");
+		}
+		//uh oh
 		else{
 			printf("Something unaccounted for occured (5)\n");
 		}
 
-
+		//retrieves next character for analysis
 		c = fgetc(stdin);
 	}	
 
