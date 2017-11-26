9ca1980725b49d7007cedc22f3ba6ab33ced03d2
Ryan P <ryan.eliopoulos@gmail.com>
2017-11-26 15:07:13 -0800

    Appears that we have a working struct searching algorithm

--- struct.c
+++ struct.c
@@ -13,68 +13,64 @@ typedef struct{
 	}Phrase;
 
 Phrase phraseList[1000];
-
-
-//
+//tracks length of phraseList
 int phraseCount = 0;
 
 
-//This works, but would need to first search phraseText for any Phrases that match the incoming string.
+//Searches data structure for a match with inputString and increments phrase count if found, otherwise appends inputString to data structure
 void receiveString(char* inputString){
 
-	//Need fnx here that checks string against phraseTexts.
-	strcpy(phraseList[phraseCount].phraseText, inputString);
-	phraseCount++;	
+	//Attempting to search through data struct and determine if a phrase is a match	
+	int j = 0;
 
-//	Attempting to search through data struct and determine if a phrase is a match	
-	//int j = 0;
-
+	//Acts as a flag indicating the data struct is still being searched for a match
+	int searchContinues = 1;	
 	//search through struct list for a phrase match (testing/writing)
-	//while(j < phraseCount){
-	//	
-	//	int i = 0;
-	//	
-	//	while(inputString[i] == phraseList[j].phraseText[i]){
-//
-//			if(inputString[i] == '\0'){
-//				printf("We've got a match!\n");
-//			}
-//			i++;	
-//		j++;
-//
-//		}
-//	}	
-
+	while((j < phraseCount) && (searchContinues)){
 	
-}
+		printf("inside struct search loop 1\n");	
+		int i = 0;
+		
+		//continues iterating over the phrases as long as they match	
+		while((inputString[i] == phraseList[j].phraseText[i]) && (searchContinues)){
 
-//Iterates through the list of phrases and prints the phraseText of each one
+			printf("inside struct search loop 2\n");
+
+			//Increments phraseList[j].count if the inputString is found to match.
+			if(inputString[i] == '\0'){
+				printf("We've got a match!\n");
+				printf("%s matches %s\n", inputString, phraseList[j].phraseText); 
+				phraseList[j].count = phraseList[j].count + 1;
+				searchContinues = 0;
+			}
+			i++;	
+		}
+		j++;
+
+	}
+
+	//searchContinues will be true if no match was found within the data structure
+	//The phrase is added to the data structure if true.
+	if(searchContinues){
+
+		//Copies the inputString into the next available slot
+		strcpy(phraseList[phraseCount].phraseText, inputString);
+
+		//Sets count value of new phrase to 1 instead of 0;
+		phraseList[phraseCount].count++;
+		phraseCount++;
+	}
+}	
+
+
+//Iterates through the list of phrases and prints the phraseText and occurence count of each
 void printPhraseList(){
 	
 	for(int p = 0;p<phraseCount;p++){
 		
-		printf("<%s>\n", phraseList[p].phraseText);
+		printf("%05d <%s>\n", phraseList[p].count, phraseList[p].phraseText);
 	}
 
 
 }
 
-//testing in above function
-//void traversephraseList(){
-//
-//	int j = 0;
-//
-//	while(j < phraseCount){
-//		
-//		int i = 0;
-//		
-//		while(inputString[i] == phraseList[j].phraseText[i]){
-//
-//			if(inputString[i] == '\0'){
-//				printf("We've got a match!\n");
-//			}	
-///
-//		}
-//	}	
-
-//}
